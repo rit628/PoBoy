71c895f9c4d0d7e77dfbc6348c30b3e740f029df
Tomas Rivera <tr4462603@gmail.com>
2026-07-13 11:45:16 -0400

    frontend: Reduce audio stream gain

--- src/frontend/gui/AudioStreamer.cpp
+++ src/frontend/gui/AudioStreamer.cpp
@@ -10,6 +10,7 @@ AudioStreamer::AudioStreamer() {
     if (!audioStream || !SDL_ResumeAudioStreamDevice(audioStream)) {
         throw std::runtime_error("Audio streamer failed to initialize: "s + SDL_GetError());
     }
+    SDL_SetAudioStreamGain(audioStream, 0.15f);
 }
 
 AudioStreamer::~AudioStreamer() {
